0489d763291caded010ff49f8728f2146f7384fb
arunshar2725 <arunshar2725@gmail.com>
2026-06-01 13:40:22 +0530

    Export Bug Fixed for pdf

--- src/main/java/com/scm/controller/ContactExportController.java
+++ src/main/java/com/scm/controller/ContactExportController.java
@@ -1,13 +1,7 @@
 package com.scm.controller;
 
-import com.scm.Repositories.ContactRepo;
-import com.scm.entities.Contact;
-import com.scm.entities.User;
-import com.scm.helpers.Helper;
-import com.scm.Services.UserService;
-
-import jakarta.servlet.http.HttpServletResponse;
-import lombok.RequiredArgsConstructor;
+import java.io.IOException;
+import java.util.List;
 
 import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.ss.usermodel.Sheet;
@@ -19,8 +13,20 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import java.io.IOException;
-import java.util.List;
+import com.itextpdf.text.Document;
+import com.itextpdf.text.PageSize;
+import com.itextpdf.text.Paragraph;
+import com.itextpdf.text.pdf.PdfPTable;
+import com.itextpdf.text.pdf.PdfWriter;
+
+import com.scm.Repositories.ContactRepo;
+import com.scm.entities.Contact;
+import com.scm.entities.User;
+import com.scm.helpers.Helper;
+import com.scm.Services.UserService;
+
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
 
 @Controller
 @RequestMapping("/user/contacts")
@@ -30,14 +36,17 @@ public class ContactExportController {
     private final ContactRepo contactRepo;
     private final UserService userService;
 
+    // ==========================
+    // EXCEL EXPORT
+    // ==========================
     @GetMapping("/export/excel")
     public void exportExcel(
             HttpServletResponse response,
             Authentication authentication) throws IOException {
 
-        String username = Helper.getEmailOfLoggedInUser(authentication);
+        String email = Helper.getEmailOfLoggedInUser(authentication);
 
-        User user = userService.getUserByEmail(username);
+        User user = userService.getUserByEmail(email);
 
         List<Contact> contacts = contactRepo.findByUser(user);
 
@@ -77,8 +86,75 @@ public class ContactExportController {
                             : "General");
         }
 
-        workbook.write(response.getOutputStream());
+        for (int i = 0; i < 4; i++) {
+            sheet.autoSizeColumn(i);
+        }
 
+        workbook.write(response.getOutputStream());
         workbook.close();
+    }
+
+    // ==========================
+    // PDF EXPORT
+    // ==========================
+    @GetMapping("/export/pdf")
+    public void exportPdf(
+            HttpServletResponse response,
+            Authentication authentication) throws Exception {
+
+        String email = Helper.getEmailOfLoggedInUser(authentication);
+
+        User user = userService.getUserByEmail(email);
+
+        List<Contact> contacts = contactRepo.findByUser(user);
+
+        response.setContentType("application/pdf");
+
+        response.setHeader(
+                "Content-Disposition",
+                "attachment; filename=contacts.pdf");
+
+        Document document = new Document(PageSize.A4);
+
+        PdfWriter.getInstance(
+                document,
+                response.getOutputStream());
+
+        document.open();
+
+        document.add(new Paragraph("Smart Contact Manager"));
+        document.add(new Paragraph(" "));
+        document.add(new Paragraph("My Contacts"));
+        document.add(new Paragraph(" "));
+
+        PdfPTable table = new PdfPTable(4);
+
+        table.addCell("Name");
+        table.addCell("Email");
+        table.addCell("Phone");
+        table.addCell("Category");
+
+        for (Contact c : contacts) {
+
+            table.addCell(
+                    c.getName() != null ? c.getName() : "");
+
+            table.addCell(
+                    c.getEmail() != null ? c.getEmail() : "");
+
+            table.addCell(
+                    c.getPhoneNumber() != null
+                            ? c.getPhoneNumber()
+                            : "");
+
+            table.addCell(
+                    c.getCategory() != null
+                            ? c.getCategory()
+                            : "General");
+        }
+
+        document.add(table);
+
+        document.close();
     }
 }
